8fa00bf774be97acbd0a75ced487cc6f4762e78a
hrissolo <hrissolo@gmail.com>
2024-01-23 14:26:04 -0500

    init

---
+++ hello.php
@@ -0,0 +1,6 @@
+<html>
+  <head><title>Hello World PHP</title></head>
+  <body>
+    <p><?php echo 'Hello World lets goooo'; ?></p>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    // Single Line comment: Hello World! 
+    echo 'Hello World'; 
+?>
+
+<?='Hello World'?>
+
+<?php
+/* We prefer to use echo. 
+    print is a little slower.
+*/
+    print 'Hello World'; 
+?>
+
+<?php 
+    $age = 34;
+
+    echo $age; // outputs 34
+?>
+
+<?php 
+    $name = "Hailey";
+    $city = "Louisville";
+    $movie = "Kill Bill";
+    $friend = "Julie";
+    $candy = "Sour patch kids";
+
+    echo "$name was excited to see $movie with her friend, $friend. 
+    $friend lives in $city & they always eat $candy together."
+    ?>
